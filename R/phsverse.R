a002916e89d36902577a57b9cffc45d585e592de
Tina Fu <yuyan.fu2@phs.scot>
2022-01-21 12:50:54 +0000

    update phsverse.R

--- R/phsverse.R
+++ R/phsverse.R
@@ -1,10 +1,6 @@
 #' \code{phsverse} package
 #'
-#' It was created with [metamakr](https://github.com/jdtrat/metamakr) and
-#' combines our PHS packages on GitHub,
-#' [phstemplates](https://github.com/Public-Health-Scotland/phstemplates),
-#' [phsmethods](https://github.com/Public-Health-Scotland/phsmethods) and
-#' [phsstyles](https://github.com/Public-Health-Scotland/phsstyles).
+#' This package is designed to make it easy to install and load multiple packages developed in PHS in a single step.
 #'
 #' See the README on
 #' \href{https://github.com/Public-Health-Scotland/phsverse#readme}{GitHub}.
